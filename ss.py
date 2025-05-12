016a0293debc2dcf5530e6ee9a58bb545eda5d6a
jitusai <74604755+jitusai@users.noreply.github.com>
2025-05-12 18:00:29 +0530

    Update ss.py

--- ss.py
+++ ss.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def home():
     return "Hello from Azure Python Web App!"
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+   app.run(host='0.0.0.0', port=8000)
